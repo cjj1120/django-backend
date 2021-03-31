d763eb6d0bea64fa96c43bed05b5c1f7a3e21b76
cjj1120 <cjj8168@gmail.com>
2021-03-31 13:17:21 +0800

    desk, url, html page

--- myblog/myproject/boards/views.py
+++ myblog/myproject/boards/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello, World!</h1>')
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'boards/post_list.html', {})
+    #render (put together) our template
+

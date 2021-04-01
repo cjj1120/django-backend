407384565d5860578ee4ef0418d9a1982801a611
cjj1120 <cjj8168@gmail.com>
2021-04-01 18:35:42 +0800

    desk lot of shit

--- myblog/myproject/boards/urls.py
+++ myblog/myproject/boards/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 #. is the current directory 
 
 
@@ -8,5 +9,7 @@ urlpatterns = [
     
     #Djangogirls: 
     path('', views.post_list, name='post_list'),
-
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
